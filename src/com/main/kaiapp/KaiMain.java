c4cc2634839aa163a776787b90f73b0389c5ea97
ruler88 <helloworld0424@gmail.com>
2014-03-09 16:42:36 -0700

    added camera ability

--- src/com/main/kaiapp/KaiMain.java
+++ src/com/main/kaiapp/KaiMain.java
@@ -1,15 +1,22 @@
 package com.main.kaiapp;
 
 import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.hardware.Camera.CameraInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class KaiMain extends Activity {
+	private final static String DEBUG_TAG = "KaiAppMain";
 	
 	private Button syncButton;
+	private Camera camera;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +33,14 @@ public class KaiMain extends Activity {
 		return true;
 	}
 	
-	
+	@Override
+	protected void onPause() {
+	    if (camera != null) {
+	      camera.release();
+	      camera = null;
+	    }
+	    super.onPause();
+	  }
 	
 	
 	private void addListenerOnButton() {
@@ -41,6 +55,30 @@ public class KaiMain extends Activity {
 	
 	private void startSync() {
 		//TODO: take camera photo and upload
+		//upload
+		cameraCap();
+	}
+	
+	private void cameraCap() {
+		if (getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+			//find front camera (or fall back to back camera)
+			int numberOfCameras = Camera.getNumberOfCameras();
+			int cameraId = -1;
+			for (int i = 0; i < numberOfCameras; i++) {
+		      CameraInfo info = new CameraInfo();
+		      Camera.getCameraInfo(i, info);
+		      cameraId = i;
+		      if (info.facing == CameraInfo.CAMERA_FACING_FRONT) {
+		        Log.d(DEBUG_TAG, "Camera found");
+		        break;
+		      }
+		    }
+			
+			camera = Camera.open(cameraId);
+			camera.takePicture(null, null, new PhotoHandler(getApplicationContext()));
+		} else {
+			Toast.makeText(this, "Camera unavailable", Toast.LENGTH_LONG).show();
+		}
 	}
 	
 	
